d656a06cc7ba28c74dfac93cacaac142d141c355
Bordin Intarma <bordin@odds.team>
2020-08-11 10:20:44 +0700

    Modified chart traffic

--- src/components/Chart/ChartTraffic/ChartTraffic.js
+++ src/components/Chart/ChartTraffic/ChartTraffic.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Chart from 'chart.js';
 import './ChartTraffic.scss';
 
 function ChartTraffic() {
 
-    const [chart,] = useState('');
     const [mainChartElements] = useState(27);
     const [mainChartData1] = useState([]);
     const [mainChartData2] = useState([]);
@@ -43,21 +42,21 @@ function ChartTraffic() {
                     'Friday', 'Saturday', 'Sunday', 'Monday', 'Thursday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
                 ],
                 datasets: [
-                    { // brandInfo
+                    {
                         data: mainChartData1,
                         label: 'Current',
                         backgroundColor: convertHex('#63c2de', 10),
                         borderColor: '#63c2de',
                         pointHoverBackgroundColor: '#fff'
                     },
-                    { // brandSuccess
+                    {
                         data: mainChartData2,
                         label: 'Previous',
                         backgroundColor: 'transparent',
                         borderColor: '#4dbd74',
                         pointHoverBackgroundColor: '#fff'
                     },
-                    { // brandDanger
+                    {
                         data: mainChartData3,
                         label: 'BEP',
                         backgroundColor: 'transparent',
@@ -108,32 +107,76 @@ function ChartTraffic() {
                 }
             }
         });
-
-
     })
 
     return (
-        <div className="card-body">
-            <div className="row">
-                <div className="col-sm-5">
-                    <h4 className="card-title mb-0">Traffic</h4>
-                    <div className="small text-muted">November 2020</div>
+        <Fragment>
+            <div className="card-body">
+                <div className="row">
+                    <div className="col-sm-5">
+                        <h4 className="card-title mb-0">Traffic</h4>
+                        <div className="small text-muted">November 2020</div>
+                    </div>
+                    <div className="col-sm-7 d-none d-md-block">
+                        <button className="btn btn-primary float-right" type="button">
+                            <i className="icon-cloud-download"></i>
+                        </button>
+                        <div className="btn-group btn-group-toggle float-right mr-3">
+                            <label btnradio="Day" className="btn btn-outline-secondary" id="option1" aria-pressed="false">Day</label>
+                            <label btnradio="Month" className="btn btn-outline-secondary active" id="option2" aria-pressed="true">Month</label>
+                            <label btnradio="Year" className="btn btn-outline-secondary" id="option3" aria-pressed="false">Year</label>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-sm-7 d-none d-md-block">
-                    <button className="btn btn-primary float-right" type="button">
-                        <i className="icon-cloud-download"></i>
-                    </button>
-                    <div className="btn-group btn-group-toggle float-right mr-3">
-                        <label btnradio="Day" className="btn btn-outline-secondary" id="option1" aria-pressed="false">Day</label>
-                        <label btnradio="Month" className="btn btn-outline-secondary active" id="option2" aria-pressed="true">Month</label>
-                        <label btnradio="Year" className="btn btn-outline-secondary" id="option3" aria-pressed="false">Year</label>
+                <div className="chart-wrapper" style={{ height: '300px', marginTop: '40px' }}>
+                    <canvas id="chartTraffic" className="chart"></canvas>
+                </div>
+            </div>
+            <div className="card-footer">
+                <div className="row text-center">
+                    <div className="col-sm-12 col-md mb-sm-2 mb-0">
+                        <div className="text-muted">Visits</div>
+                        <strong>29.703 Users (40%)</strong>
+                        <div className="progress progress-xs mt-2">
+                            <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="40" className="progress-bar bg-success" role="progressbar" style={{ width: '40%' }}>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md mb-sm-2 mb-0">
+                        <div className="text-muted">Unique</div>
+                        <strong>24.093 Users (20%)</strong>
+                        <div className="progress progress-xs mt-2">
+                            <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="20" className="progress-bar bg-info" role="progressbar" style={{ width: '20%' }}>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md mb-sm-2 mb-0">
+                        <div className="text-muted">Pageviews</div>
+                        <strong>78.706 Views (60%)</strong>
+                        <div className="progress progress-xs mt-2">
+                            <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="60" className="progress-bar bg-warning" role="progressbar" style={{ width: '60%' }}>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md mb-sm-2 mb-0">
+                        <div className="text-muted">New Users</div>
+                        <strong>22.123 Users (80%)</strong>
+                        <div className="progress progress-xs mt-2">
+                            <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="80" className="progress-bar bg-danger" role="progressbar" style={{ width: '80%' }}>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md mb-sm-2 mb-0">
+                        <div className="text-muted">Bounce Rate</div>
+                        <strong>40.15%</strong>
+                        <div className="progress progress-xs mt-2">
+                            <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="40" className="progress-bar" role="progressbar" style={{ width: '40%' }}>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="chart-wrapper" style={{ height: '300px', marginTop: '40px' }}>
-                <canvas id="chartTraffic" className="chart"></canvas>
-            </div>
-        </div>
+        </Fragment>
     )
 }
 
